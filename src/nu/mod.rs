c3d311f1428e4c74e5d480c5dd17065e45448601
Joseph T Lyons <JosephTLyons@gmail.com>
2024-08-23 21:00:14 -0400

    Add missing trait methods

--- src/nu/mod.rs
+++ src/nu/mod.rs
@@ -17,6 +17,10 @@ impl Plugin for Units {
     fn commands(&self) -> Vec<Box<dyn PluginCommand<Plugin = Self>>> {
         vec![Box::new(Units)]
     }
+
+    fn version(&self) -> String {
+        env!("CARGO_PKG_VERSION").into()
+    }
 }
 
 impl SimplePluginCommand for Units {
